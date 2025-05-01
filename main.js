d07077964b9f65f99a789910c72baaad1443b7df
lemon10041004 <qaz28192022@gmail.com>
2025-05-01 23:54:41 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-// 正式功能版 main.js
+
+function generate() {
+    const result = [];
+    while (result.length < 1) {
+        const num = Math.floor(Math.random() * 49) + 1;
+        if (!result.includes(num)) result.push(num);
+    }
+    const now = new Date().toLocaleString('zh-TW');
+    document.getElementById('result').innerHTML = '預測號碼：' + result.join(', ') + '<br>預測時間：' + now;
+}
