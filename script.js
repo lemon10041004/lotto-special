f0919f8dff5be1a41291bcf21359c9c0df4a4314
lemon10041004 <qaz28192022@gmail.com>
2025-05-02 00:07:54 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function predict() {
-  let results = [];
-  while (results.length < 24) {
-    let num = Math.floor(Math.random() * 49) + 1;
-    results.push(num);
-  }
-  document.getElementById("result").innerText = "預測號碼（24組）：" + results.join(", ");
-}
